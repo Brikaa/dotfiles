af9871fc2cfb22cbb58210fe08e1cb3306ac4e09
Omar Brikaa <brikaaomar@gmail.com>
2024-02-14 17:51:11 +0200

    Presentation script with layout.sh

--- .screenlayout/layout.sh
+++ .screenlayout/layout.sh
@@ -1,5 +1,10 @@
 i3_monitor_config=""
 case "$1" in
+    presentation)
+        xrandr --auto
+        xrandr --output HDMI-1 --same-as eDP-1 --mode 1920x1080 --auto
+        i3_monitor_config="off"
+        ;;
     auto)
         xrandr --auto
         i3_monitor_config="off"
--- .screenlayout/presentation.sh
+++
@@ -1,1 +0,0 @@
-xrandr --output HDMI-1 --same-as eDP-1 --mode 1920x1080 --auto
